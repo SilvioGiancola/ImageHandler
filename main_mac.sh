2dbfabbfe16d754a9d8c6262f19a50ce651256a7
SilvioGiancola <silvio.giancola@gmail.com>
2019-01-22 10:54:51 +0300

    added lxml on conda

--- main_mac.sh
+++ main_mac.sh
@@ -4,6 +4,6 @@ cd ~/git/ImageHandler
 conda --version
 conda create -n pyqt
 source activate pyqt
-conda install python pyqt pylint pep8 flake8 yapf
+conda install python pyqt pylint pep8 flake8 yapf lxml
 conda install -c menpo opencv
 python main.py
--- main_win.bat
+++ main_win.bat
@@ -2,6 +2,6 @@ echo "Image Handler starter"
 call conda --version
 call conda create -n pyqt 
 call activate pyqt
-call conda install python pyqt pylint pep8 flake8 yapf opencv
+call conda install python pyqt pylint pep8 flake8 yapf lxml opencv
 python main.py
 PAUSE
