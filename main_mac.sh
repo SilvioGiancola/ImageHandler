baa074ce812ecc00fa8df7fb96a40df5e7018355
SilvioGiancola <silvio.giancola@kaust.edu.sa>
2019-01-23 14:09:23 +0300

    compile binaries for win

--- main_mac.sh
+++ main_mac.sh
@@ -4,7 +4,7 @@ cd $( dirname "${BASH_SOURCE[0]}" )
 conda --version
 # source deactivate
 # conda remove --name pyqt --all
-conda create -n pyqt python lxml
+conda create -n pyqt python lxml pylint pep8 flake8 yapf
 source activate pyqt 
 conda install -c menpo opencv
 pip install PyQt5
@@ -12,6 +12,7 @@ echo "Run Image Handler"
 python main.py
 echo "Build Image Handler"
 pip install pyinstaller
+# conda env export > env_mac.yml
 # rm -r dist
 # rm -r build
 # rm main.spec
--- main_win.bat
+++ main_win.bat
@@ -1,7 +1,17 @@
 echo "Image Handler starter"
 call conda --version
-call conda create -n pyqt 
+REM call deactivate
+REM call conda remove --name pyqt --all
+call conda create -n pyqt python lxml pylint pep8 flake8 yapf opencv
 call activate pyqt
-call conda install python pyqt pylint pep8 flake8 yapf lxml opencv
+pip install PyQt5
 python main.py
+REM PAUSE
+pip install pyinstaller
+REM call conda env export > env_win.yml
+REM rm -r dist
+REM rm -r build
+REM rm main.spec
+pyinstaller -F main.py
+start dist/main
 PAUSE
