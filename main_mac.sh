d8d3f2c767e285f5cbb442f60c8bf43a16f50025
SilvioGiancola <silvio.giancola@gmail.com>
2019-01-23 13:51:07 +0300

    binaries for mac

--- main_mac.sh
+++ main_mac.sh
@@ -2,8 +2,18 @@
 echo "Image Handler starter"
 cd $( dirname "${BASH_SOURCE[0]}" )
 conda --version
-conda create -n pyqt
-source activate pyqt
-conda install python pyqt pylint pep8 flake8 yapf lxml
+# source deactivate
+# conda remove --name pyqt --all
+conda create -n pyqt python
+source activate pyqt lxml
 conda install -c menpo opencv
+pip install PyQt5
+echo "Run Image Handler"
 python main.py
+echo "Build Image Handler"
+pip install pyinstaller
+rm -r dist
+rm -r build
+rm main.spec
+pyinstaller -F main.py
+./dist/main
